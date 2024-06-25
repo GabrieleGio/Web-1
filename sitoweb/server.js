c89cb8a86ab1a85e81083bc7c263785f37788d1e
Nio4124 <antoniozottola41204@gmail.com>
2024-06-25 19:04:16 +0200

    gg

--- sitoweb/server.js
+++ sitoweb/server.js
@@ -8,10 +8,35 @@ var iPortaTcp = 4201;
 var sIpAddress = "127.0.0.1"
 
 app.listen(iPortaTcp,sIpAddress, () => console.log('API is running on http://' + sIpAddress + ':' + iPortaTcp));
-//const bodyParser = require('body-parser');
-//app.use(bodyParser.urlencoded({ extended: true }));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 //PRIMA ROUTE
 app.get('', (req, res) => {
     console.log("Mi hai chiesto la pagina iniziale");
-    res.sendFile("index.html", { root: './htdocs' });
+    res.sendFile('index.html', { root: './htdocs' });
+});
+
+//SECONDA ROUTE
+app.get('/registrati.html', (req, res) => {
+    console.log("Mi hai chiesto la pagina di registrazione");
+    res.sendFile('registrati.html', { root: './htdocs' });
     });
+
+//TERZA ROUTE
+app.get('/accedi.html', (req, res) => {
+    console.log("Mi hai chiesto la pagina di accesso");
+    res.sendFile('accedi.html', { root: './htdocs' });
+    });
+
+//QUARTA ROUTE
+app.get('/index.html', (req, res) => {
+    console.log("Mi hai chiesto la pagina iniziale");
+    res.sendFile('index.html', { root: './htdocs' });
+    });
+
+//FORM ROUTE
+app.get('/gestiscidatiform', (req, res) => {
+    console.log("Mi hai chiesto la pagina della form");
+    res.send("<html><body>Ciao " + req.query.fname + "</body</html>");
+    });
+
